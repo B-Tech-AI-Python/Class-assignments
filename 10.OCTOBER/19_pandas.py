02bf3f814bc2a3e1d7e3af51223917689c59cf01
Ishani Kathuria <ishani@kathuria.net>
2020-10-22 10:11:49 +0530

    update

--- 10.OCTOBER/19_pandas.py
+++ 10.OCTOBER/19_pandas.py
@@ -17,5 +17,36 @@ df1 = pd.DataFrame({'A': d1,
                     'D': ['Who', 'What', 'Why', 'When']})
 
 print(df1)
-print()
 
+print()
+print(df1.head())
+
+print()
+print(df1.tail())
+
+print()
+print(df1.shape)
+
+print()
+print(df1.index)
+
+print()
+print(df1.describe())
+
+print()
+print(df1.columns)
+
+print()
+print(df1.values)
+
+print()
+print(df1.sort_index(axis=1))
+
+print()
+print(df1.sort_values('D'))
+
+print()
+print(df1.ndim)
+
+
+print()
