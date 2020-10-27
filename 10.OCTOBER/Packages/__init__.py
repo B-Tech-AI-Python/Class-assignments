ffea375407ac3c7822ffde791a76ddffa324ca3a
Ishani Kathuria <ishani@kathuria.net>
2020-10-27 11:40:18 +0530

    update

---
+++ 10.OCTOBER/Packages/__init__.py
@@ -0,0 +1,10 @@
+from cars import bmw
+from cars import nissan
+
+# Create an object of Bmw class & call its method 
+ModBMW = bmw.Bmw()
+ModBMW.outModels()
+
+# Create an object of Nissan class & call its method 
+ModNissan = nissan.Nissan()
+ModNissan.outModels()
