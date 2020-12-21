92be4a1e8fca193600bab1bde4da602192c555ce
Ishani Kathuria <ishani@kathuria.net>
2020-12-21 11:43:26 +0530

    first lab of sem 4

---
+++ sem 4/Lab1/12_21.py
@@ -0,0 +1,26 @@
+# arithmetic
+# comparison
+# assignment
+# logical
+# membership
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+user_name = input("\nEnter your name: ")
+print("\nHello" + " " + user_name + "!")
+
+count = 0
+
+for letter in vowels:
+    if letter in user_name.lower():
+        count += 1
+
+if count > 0:
+    print(f"\nYou have {count} vowels in your name.")
+
+else:
+    print("\nYou have no vowels in your name.")
+
+if len(user_name) >= 6:
+    print("\nYou have a long name!")
+
