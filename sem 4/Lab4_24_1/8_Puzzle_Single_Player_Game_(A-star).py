8c12137ae083f1829e6d21bae242a8a633ab94ff
Ishani Kathuria <ishani@kathuria.net>
2021-02-07 22:53:37 +0530

    update

--- sem 4/Lab4_24_1/8_Puzzle_Single_Player_Game_(A-star).py
+++ sem 4/Lab4_24_1/8_Puzzle_Single_Player_Game_(A-star).py
@@ -3,19 +3,33 @@ from queue import PriorityQueue
 
 
 class Puzzle:
+    """Puzzle class to store different states and actions.
+
+    Attributes:
+        goal_state (list): list of goal state.
+        num_of_instances (int): keep track of number of instances.
+        heuristic: heuristic value (h).
+        evaluation_function: evaluated value (f).
+        needs_hueristic (bool): True or False.
+        state (list): current state.
+        parent (list): parent state.
+        action: action.
+        path_cost: path cost (g).
+
+    """
     # Setting the goal state of 8-puzzle
-    goal_state = [1,2,3,8,0,4,7,6,5]
-    # Setting up the members of a class
+    goal_state = [1, 2, 3, 8, 0, 4, 7, 6, 5]
+    num_of_instances = 0
+
     heuristic = None
     evaluation_function = None
     needs_hueristic = False
-    num_of_instances = 0
 
     def __init__(self, state, parent, action, path_cost, needs_hueristic=False):
         self.parent = parent
         self.state = state
         self.action = action
-        #TODO: calculate the path_cost as the sum of its parent cost and path_cost
+        # calculating the path_cost as the sum of its parent cost and path_cost
         if parent:
             self.path_cost = parent.path_cost + path_cost
         else:
@@ -29,17 +43,17 @@ class Puzzle:
 
         # incrementing the number of instance by 1
         Puzzle.num_of_instances += 1
-    
-    # method used to display a state of 8-puzzle
+
     def __str__(self):
+        """Method used to display a state of 8-puzzle."""
         return str(self.state[0:3])+'\n'+str(self.state[3:6])+'\n'+str(self.state[6:9])
 
-    # method used to generate a heuristic value
     def generate_heuristic(self):
+        """Method used to generate a heuristic value."""
         self.heuristic = 0
-        for num in range(1,9):
-            # calculate the heuristic value as manhattan distance which is the absolute 
-            # difference between current state and goal state. 
+        for num in range(1, 9):
+            # calculate the heuristic value as manhattan distance which is the absolute
+            # difference between current state and goal state.
             # Use index() method to get the index of num in state
             distance = abs(self.state.index(num) - self.goal_state.index(num))
             i = int(distance / 3)
@@ -47,14 +61,14 @@ class Puzzle:
             self.heuristic = self.heuristic + i + j
 
     def goal_test(self):
-        # include a condition to compare the current state with the goal state
+        """Method used to compare the current state with the goal state."""
         if self.state == self.goal_state:
             return True
         return False
 
     @staticmethod
-    def find_legal_actions(i,j):
-        # find the legal actions as Up, Down, Left, Right based on each cell of state
+    def find_legal_actions(i, j):
+        """Method ro find legal actions on each cell of state."""
         legal_action = ['U', 'D', 'L', 'R']
         if i == 0:  # up is disable
             legal_action.remove('U')
@@ -67,16 +81,15 @@ class Puzzle:
 
         return legal_action
 
-    #method to generate the child of the current state of the board
     def generate_child(self):
-        # create an empty list
+        """Method to generate the child of the current state of the board."""
         children = []
         x = self.state.index(0)
-        #TODO: generate the row (i) & col (j) position based on the current index of 0 on the board 
+        # generate the row (i) & col (j) position based on the current index of 0 on the board
         i = int(x / 3)
         j = int(x % 3)
         # call the method to find the legal actions based on i and j values
-        legal_actions = self.find_legal_actions(i,j)
+        legal_actions = self.find_legal_actions(i, j)
 
         for action in legal_actions:
             new_state = self.state.copy()
@@ -101,83 +114,105 @@ class Puzzle:
                 new_state[x], new_state[x+1] = new_state[x+1], new_state[x]
 
             # Append the new_state of Puzzle object with parent, action,path_cost is 1, its needs_hueristic flag
-            children.append(Puzzle(new_state, self, action, 1, self.needs_hueristic))
+            children.append(
+                Puzzle(new_state, self, action, 1, self.needs_hueristic))
 
         # return the children
         return children
-    
-    #method to find the solution
+
     def find_solution(self):
+        """Method to find the solution."""
         solution = []
+        solution_states = []
+
         solution.append(self.action)
+        solution_states.append(self)
+
         path = self
         while path.parent != None:
             path = path.parent
             solution.append(path.action)
+            solution_states.append(path)
+
         solution = solution[:-1]
+        solution_states = solution_states[:-1]
+
         solution.reverse()
-        return solution
+        solution_states.reverse()
+
+        return solution, solution_states
 
 
-#method for A-star search
-#TODO: pass the initial_state as parameter to the breadth_first_search method
 def Astar_search(initial_state):
-    count=0
-    #TODO: create an empty list of explored nodes
-    explored=[]
-    #TODO: create a instance of Puzzle as initial_state, None, None, 0, True
-    start_node=Puzzle(initial_state, None, None, 0, True)
+    """Function for A-star search.
+    
+    Attributes:
+        initial_state: initial state.
+
+    """
+    count = 0
+    explored = []
+
+    start_node = Puzzle(initial_state, None, None, 0, True)
+    print(start_node)
+
     q = PriorityQueue()
-    #TODO: put a tuple with start_node.evaluation_function, count, start_node into PriorityQueue
     q.put((start_node.evaluation_function, count, start_node))
 
     while not q.empty():
-        #TODO: get the current node of a queue. Use the get() method of Queue
-        node=q.get()
-        #TODO: extract the current node of a PriorityQueue based on the index of a tuple. 
-        #Refer a tuple format put in PriorityQueue  
-        node=node[2]
-        #TODO: Append the state of node in the explored list as node.state
+        # get the current node of a queue
+        node = q.get()
+        # extract the current node of a PriorityQueue
+        node = node[2]
+
+        # Append the state of node in the explored list as node.state
         explored.append(node.state)
         if node.goal_test():
             return node.find_solution()
-        #TODO: call the generate_child method to generate the child node of current node
-        children=node.generate_child()
+
+        # call the generate_child method to generate the child node of current node
+        children = node.generate_child()
         for child in children:
             if child.state not in explored:
                 count += 1
-                #TODO: put a tuple with child.evaluation_function, count, child into PriorityQueue
+                # put a tuple with child.evaluation_function, count, child into PriorityQueue
                 q.put((child.evaluation_function, count, child))
     return
 
 
 # Start executing the 8-puzzle with setting up the initial state
 # Here we have considered 3 initial state intitalized using state variable
-state=[[1, 3, 4,
-        8, 6, 2,
-        7, 0, 5],
+state = [[1, 3, 4,
+          8, 6, 2,
+          7, 0, 5],
 
-       [2, 8, 1,
-        0, 4, 3,
-        7, 6, 5],
+         [2, 8, 1,
+          0, 4, 3,
+          7, 6, 5],
 
-       [2, 8, 1,
-        4, 6, 3,
-        0, 7, 5]]
-#Iterate over number of initial_state
-for i in range(0,3):
-    # Initialize the num_of_instances to zero
+         [2, 8, 1,
+          4, 6, 3,
+          0, 7, 5]]
+
+for i in range(0, 3):
     Puzzle.num_of_instances = 0
-    # Set t0 to current time
+
     t0 = time()
+
     astar = Astar_search(state[i])
-    # Get the time t1 after executing the breadth_first_search method
+
     t1 = time() - t0
-    print('A*:',astar)
+
+    print("Solution states:")
+    for i in range(len(astar[1])):
+        print("state", i+1)
+        print(astar[1][i])
+        print()
+    print()
+
+    print('A*:', astar[0])
+
     print('space:', Puzzle.num_of_instances)
     print('time:', t1)
     print()
     print('------------------------------------------')
-
-# Show the path in traversing the A-star algorithm of each state from intial_state to goal state.
-
