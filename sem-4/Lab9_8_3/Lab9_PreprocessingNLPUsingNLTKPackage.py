82bb46886f2af0d9c124d843d97e1affca68c9cb
Ishani Kathuria <ishani@kathuria.net>
2021-04-02 21:31:44 +0530

    update

--- sem-4/Lab9_8_3/Lab9_PreprocessingNLPUsingNLTKPackage.py
+++ sem-4/Lab9_8_3/Lab9_PreprocessingNLPUsingNLTKPackage.py
@@ -1,51 +1,31 @@
-"""Lab 9:Preprocessing Techniques in NLP Using NLTK package
-
-In this lab, we will be exploring how to preprocess tweets for sentiment analysis.
-
-Setup
-In this lab, we will be using the [Natural Language Toolkit (NLTK)](http://www.nltk.org/howto/twitter.html) package, an open-source Python library for natural language processing. It has modules for collecting, handling, and processing Twitter data.
-For this exercise, we will use a Twitter dataset that comes with NLTK. This dataset has been manually annotated and serves to establish baselines for models quickly. Let us import them now as well as a few other libraries we will be using.
-
-## About the Twitter dataset
-The sample dataset from NLTK is separated into positive and negative tweets. It contains 5000 positive tweets and 5000 negative tweets exactly. The exact match between these classes is not a coincidence. The intention is to have a balanced dataset. That does not reflect the real distributions of positive and negative classes in live Twitter streams. It is just because balanced datasets simplify the design of most computational methods that are required for sentiment analysis. However, it is better to be aware that this balance of classes is artificial. In a local computer however, you can download the data by doing:
-
-## Preprocess raw text for Sentiment analysis
-Data preprocessing is one of the critical steps in any machine learning project. It includes cleaning and formatting the data before feeding into a machine learning algorithm. For NLP, the preprocessing steps are comprised of the following tasks:
-    
-### Tokenizing the string
-To tokenize means to split the strings into individual words without blanks or tabs. In this same step, we will also convert each word in the string to lower case. The [tokenize](https://www.nltk.org/api/nltk.tokenize.html#module-nltk.tokenize.casual) module from NLTK allows us to do these easily:
-
-### Lowercasing
-
-### Removing stop words and punctuation
-The next step is to remove stop words and punctuation. Stop words are words that don't add significant meaning to the text. You'll see the list provided by NLTK when you run the cells below.
-
-### Stemming
-Stemming is the process of converting a word to its most general form, or stem. This helps in reducing the size of our vocabulary.
-Consider the words: 
-* **learn**
-* **learn**ing
-* **learn**ed
-* **learn**t
-All these words are stemmed from its common root **learn**. However, in some cases, the stemming process produces words that are not correct spellings of the root word. For example, **happi** and **sunni**. That's because it chooses the most common stem for related words. For example, we can look at the set of words that comprises the different forms of happy:
-* **happ**y
-* **happi**ness
-* **happi**er
-We can see that the prefix **happi** is more commonly used. We cannot choose **happ** because it is the stem of unrelated words like **happen**.
-NLTK has different modules for stemming and we will be using the [PorterStemmer](https://www.nltk.org/api/nltk.stem.html#module-nltk.stem.porter) module which uses the [Porter Stemming Algorithm](https://tartarus.org/martin/PorterStemmer/). Let's see how we can use it in the cell below.
-"""
-
+# Import the necessary libraries
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from IPython.core.display import display, HTML
 import nltk                                # Python library for NLP
 from nltk.corpus import twitter_samples    # sample Twitter dataset from NLTK
-import matplotlib.pyplot as plt            # library for visualization
-import random                              # pseudo-random number generator
-import numpy as np
+# module for stop words that come with NLTK
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer        # module for stemming
+from nltk.tokenize import TweetTokenizer   # module for tokenizing strings
 
+import pandas as pd
+import numpy as np
+import random                              # pseudo-random number generator
+import re                                  # library for regular expression operations
+import string                              # for string operations
+import matplotlib.pyplot as plt            # library for visualization
+import seaborn as sns                      # library for visualization
+
+
+# download sample twitter dataset
 nltk.download('twitter_samples')
 
-# select the set of positive and negative tweets
 all_positive_tweets = twitter_samples.strings('positive_tweets.json')
 all_negative_tweets = twitter_samples.strings('negative_tweets.json')
+
 
 print('Number of positive tweets: ', len(all_positive_tweets))
 print('Number of negative tweets: ', len(all_negative_tweets))
@@ -53,8 +33,7 @@ print('Number of negative tweets: ', len(all_negative_tweets))
 print('\nThe type of all_positive_tweets is: ', type(all_positive_tweets))
 print('The type of a tweet entry is: ', type(all_negative_tweets))
 
-# PLOT the positive and negative tweets in a pie-chart
-# Declare a figure with a custom size
+
 fig = plt.figure(figsize=(6, 6))
 
 # labels for the two classes
@@ -73,63 +52,54 @@ plt.axis('equal')
 # Display the chart
 plt.show()
 
-# Display a random tweet from positive and negative tweet.
-# The size of positive and negative tweets are 5000 each. 
-# Generate a random number between 0 and 5000 using random.randint()
-# print positive in greeen
-print('\033[92m' + all_positive_tweets[random.randint(0,5000)])
 
-# print negative in red
-print('\033[91m' + all_negative_tweets[random.randint(0,5000)])
+# print a random tweet from positive and negative tweet
+# positive in green, negative in red
+print('\033[92m' + all_positive_tweets[random.randint(0, 5000)])
+print('\033[91m' + all_negative_tweets[random.randint(0, 5000)])
 
 
-# Our selected sample. Complex enough to exemplify each step
+# a complex sample is selected
 tweet = all_positive_tweets[2277]
 print(tweet)
 
-# download the stopwords from NLTK
-nltk.download('stopwords')
-
-#Import the necessary libraries
-import re                                  # library for regular expression operations
-import string                              # for string operations
-
-from nltk.corpus import stopwords          # module for stop words that come with NLTK
-from nltk.stem import PorterStemmer        # module for stemming
-from nltk.tokenize import TweetTokenizer   # module for tokenizing strings
 
 print('\033[92m' + tweet)
 print('\033[94m')
 
 # remove old style retweet text "RT"
-tweet2 = re.sub(r'^RT[\s]+', '', tweet)
+tweet = re.sub(r'^RT[\s]+', '', tweet)
 
 # remove hyperlinks
-tweet2 = re.sub(r'https?:\/\/.*[\r\n]*', '', tweet2)
+tweet = re.sub(r'https?:\/\/.*[\r\n]*', '', tweet)
 
 # remove hashtags
-# only removing the hash # sign from the word
-tweet2 = re.sub(r'#', '', tweet2)
+tweet = re.sub(r'#', '', tweet)
 
-print(tweet2)
+print(tweet)
 
-print()
-print('\033[92m' + tweet2)
+
+print('\033[92m' + tweet)
 print('\033[94m')
 
 # instantiate tokenizer class
 tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True,
-                               reduce_len=True)
+                           reduce_len=True)
 
 # tokenize tweets
-tweet_tokens = tokenizer.tokenize(tweet2)
+tweet_tokens = tokenizer.tokenize(tweet)
 
 print()
 print('Tokenized string:')
 print(tweet_tokens)
 
-#Import the english stop words list from NLTK
-stopwords_english = stopwords.words('english') 
+
+# download the stopwords from NLTK
+nltk.download('stopwords')
+
+
+# import the english stop words list from NLTK
+stopwords_english = stopwords.words('english')
 
 print('Stop words\n')
 print(stopwords_english)
@@ -137,38 +107,36 @@ print(stopwords_english)
 print('\nPunctuation\n')
 print(string.punctuation)
 
-print()
+
 print('\033[92m')
 print(tweet_tokens)
 print('\033[94m')
 
-# Create the empty list to store the clean tweets after removing stopwords and punctuation
 tweets_clean = []
 
 # Remove stopwords and punctuation from the tweet_tokens
-for word in tweet_tokens: # Go through every word in your tokens list
+for word in tweet_tokens:  # Go through every word in your tokens list
     if (word not in stopwords_english and  # remove stopwords
-        word not in string.punctuation):  # remove punctuation
-        
+            word not in string.punctuation):  # remove punctuation
+
         # Append the clean word in the tweets_clean list
         tweets_clean.append(word)
 
 print('removed stop words and punctuation:')
 print(tweets_clean)
 
-print()
+
 print('\033[92m')
 print(tweets_clean)
 print('\033[94m')
 
 # Instantiate stemming class
-stemmer = PorterStemmer() 
+stemmer = PorterStemmer()
 
-tweets_stem = [] 
+tweets_stem = []
 
 for word in tweets_clean:
-    stem_word = stemmer.stem(word)  # stemming word
-
+    stem_word = stemmer.stem(word)
     tweets_stem.append(stem_word)
 
 print('stemmed words:')
@@ -177,13 +145,14 @@ print(tweets_stem)
 
 def process_tweet(tweet):
     """Process tweet function.
-    Input:
-        tweet: a string containing a tweet
-    Output:
-        tweets_clean: a list of words containing the processed tweet
+    Args:
+        tweet: a string containing a tweet.
+    Returns:
+        tweets_clean: a list of words containing the processed tweet.
 
     """
     stemmer = PorterStemmer()
+
     stopwords_english = stopwords.words('english')
     # remove stock market tickers like $GE
     tweet = re.sub(r'\$\w*', '', tweet)
@@ -192,8 +161,8 @@ def process_tweet(tweet):
     # remove hyperlinks
     tweet = re.sub(r'https?:\/\/.*[\r\n]*', '', tweet)
     # remove hashtags
-    # only removing the hash # sign from the word
     tweet = re.sub(r'#', '', tweet)
+
     # tokenize tweets
     tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True,
                                reduce_len=True)
@@ -221,105 +190,114 @@ print('\033[94m')
 tweets_stem = process_tweet(tweet)
 
 print('preprocessed tweet:')
-print(tweets_stem) # Print the result
+print(tweets_stem)  # Print the result
 
-# Concatenate the lists, 1st part is the positive tweets followed by the negative
+
 tweets = all_positive_tweets + all_negative_tweets
-
-# let's see how many tweets we have
 print("Number of tweets: ", len(tweets))
 
-# make a numpy array representing labels of the tweets
-labels = np.append(np.ones((len(all_positive_tweets))), np.zeros((len(all_negative_tweets))))
+
+labels = np.append(np.ones((len(all_positive_tweets))),
+                   np.zeros((len(all_negative_tweets))))
+labels
 
 
 def build_freqs(tweets, ys):
-    """Build frequencies.
-    Input:
-        tweets: a list of tweets
-        ys: an m x 1 array with the sentiment label of each tweet
-            (either 0 or 1)
-    Output:
-        freqs: a dictionary mapping each (word, sentiment) pair to its
-        frequency
+    """Build a word frequency dictionary.
+    Args:
+        tweets: a list of tweets.
+        ys: an m x 1 array with the sentiment label of each tweet (either 0 or 1).
+    Returns:
+        pos_freqs: a dictionary mapping each word with positive sentiment to its frequency.
+        neg_freqs: a dictionary mapping each word with negative sentiment to its frequency.
     """
-    # Convert np array to list since zip needs an iterable.
-    # The squeeze is necessary or the list ends up with one element.
-    # Also note that this is just a NOP if ys is already a list.
     yslist = np.squeeze(ys).tolist()
 
-    # Start with an empty dictionary and populate it by looping over all tweets
-    # and over all processed words in each tweet.
-    freqs = {}
+    pos_freqs = {}
+    neg_freqs = {}
     for y, tweet in zip(yslist, tweets):
-        # Iterate over all the words returned by calling the process_tweet() function for each tweet
         for word in process_tweet(tweet):
-            pair = (word, y)
-            # If pair matches in the dictionary, then increment the count of corresponding pair.
-            if pair in freqs:
-                freqs[pair] += 1
+            if word in pos_freqs and y == 1:
+                pos_freqs[word] += 1
+
+            elif word in neg_freqs and y == 0:
+                neg_freqs[word] += 1
+
             else:
-                # If pair does not matches in the dictionary, then set the count of corresponding pair as 1.
-                freqs[pair] = 1
+                if y == 1:
+                    pos_freqs[word] = 1
+                elif y == 0:
+                    neg_freqs[word] = 1
 
-    # Return the dictionary
-    return freqs
-
-
-# Call the build_freqs function to create frequency dictionary based on tweets and labels
-freqs = build_freqs(tweets, labels)
-
-# Display the data type of freqs
-print(f'type(freqs) = {type(freqs)}')
-
-# Display the length of the dictionary
-print(f'len(freqs) = {len(freqs)}')
+    return pos_freqs, neg_freqs
 
 
-# print all the key-value pair of frequency dictionary
-for key, value in freqs.items():
-    print(key, value)
+pos_freqs, neg_freqs = build_freqs(tweets, labels)
+
+print(f'types = {type(pos_freqs)}, {type(neg_freqs)}')
+
+print(f'lengths = {len(pos_freqs)}, {len(neg_freqs)}')
 
 
-# Select some words to appear in the report. we will assume that each word is unique (i.e. no duplicates)
-keys = ['happi', 'merri', 'nice', 'good', 'bad', 'sad',
-        'mad', 'best', 'pretti', '❤', ':)', ':(', '😒',
-        '😬', '😄', '😍', '♛', 'song', 'idea', 'power',
-        'play', 'magnific']
+def display_side_by_side(dfs: list, captions: list):
+    """Display pandas tables side by side.
+    Args:
+        dfs: list of pandas.DataFrame
+    """
+    output = ""
+    combined = dict(zip(captions, dfs))
+    for caption, df in combined.items():
+        output += df.style.set_table_attributes(
+            "style='display:inline'").set_caption(caption)._repr_html_()
+        output += "\xa0\xa0\xa0"
+    display(HTML(output))
 
-# Create the empty list as list representing our table of word counts, where 
-# each element consist of a sublist with this pattern: [<word>, <positive_count>, <negative_count>].
+
+freq_df = pd.DataFrame.from_dict({'positive_frequency': pos_freqs,
+                                  'negative_frequency': neg_freqs})
+
+top5_pos = freq_df.sort_values('positive_frequency', ascending=False).head(5)
+top5_neg = freq_df.sort_values('negative_frequency', ascending=False).head(5)
+
+display_side_by_side([top5_pos, top5_neg], ['top5_pos', 'top5_neg'])
+
+
+# select some words to appear in the report
+keys = [':)', ':-)', ':D', 'thank', 'love', ':(', ':-(', 'i\'m',
+        '...', 'miss', 'happi', 'merri', 'nice', 'good', 'bad',
+        'sad', 'mad', 'best', 'pretti', '❤', '😒', '😬', '😄',
+        '😍', '♛', 'song', 'idea', 'power', 'play', 'magnific']
+
 data = []
 
-# Iterate over each word in keys
 for word in keys:
-    # initialize positive and negative counts
     pos = 0
     neg = 0
-    
+
     # retrieve number of positive counts
-    if (word, 1) in freqs:
-        pos = freqs[(word, 1)]
-        
+    if word in pos_freqs:
+        pos = pos_freqs[word]
+
     # retrieve number of negative counts
-    if (word, 0) in freqs:
-        neg = freqs[(word, 0)]
-        
+    if word in neg_freqs:
+        neg = neg_freqs[word]
+
     # append the word counts to the table
     data.append([word, pos, neg])
-    
+
 data
 
-fig, ax = plt.subplots(figsize = (8, 8))
+
+fig, ax = plt.subplots(figsize=(8, 8))
 
 # convert positive raw counts to logarithmic scale. we add 1 to avoid log(0)
-x = np.log([x[1] + 1 for x in data])  
+x = np.log([x[1] + 1 for x in data])
 
 # do the same for the negative counts
-y = np.log([x[2] + 1 for x in data]) 
+y = np.log([x[2] + 1 for x in data])
 
 # Plot a dot for each pair of words
-ax.scatter(x, y)  
+ax.scatter(x, y)
 
 # assign axis labels
 plt.xlabel("Log Positive count")
@@ -329,35 +307,50 @@ plt.ylabel("Log Negative count")
 for i in range(0, len(data)):
     ax.annotate(data[i][0], (x[i], y[i]), fontsize=12)
 
-ax.plot([0, 9], [0, 9], color = 'red') # Plot the red line that divides the 2 areas.
+# Plot the red line that divides the 2 areas.
+ax.plot([0, 9], [0, 9], color='red')
 plt.show()
 
 
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-
 X = tweets
 y = labels
-print(len(tweets), len(labels))
+print('Number of tweets:', len(tweets))
 
 
 vectorizer = CountVectorizer(
-    analyzer = 'word',
-    lowercase = False,
+    analyzer='word',
+    lowercase=False,
 )
 
 features = vectorizer.fit_transform(
     tweets
 )
 
-features_nd = features.toarray() # for easy usage
+features_nd = features.toarray()  # for easy usage
 
 
-X_train, X_test, y_train, y_test = train_test_split(features_nd, y, test_size=0.33, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    features_nd, y, test_size=0.2, random_state=30)
 
-clf = LogisticRegression(random_state=0).fit(X_train, y_train)
 
-clf.score(X, y)
+print('Number of training tweets:', len(X_train))
+print('Number of testing tweets:', len(X_test))
+
+
+clf = LogisticRegression(solver='lbfgs').fit(X_train, y_train)
+
 
 y_pred = clf.predict(X_test)
+
+
+print('CLASSIFICATION REPORT\n', classification_report(y_test, y_pred))
+
+
+print('\nCONFUSION MATRIX\n')
+sns.heatmap(confusion_matrix(y_test, y_pred), annot=True)
+
+
+print('ACCURACY SCORE:', accuracy_score(y_test, y_pred))
+
+
+pd.DataFrame({'True values': y_test, 'Predicted values': y_pred})
